66703cd71e9be3acc255fe40cbadd7b012db9c96
devfer-sys <franklin333f@gmail.com>
2026-01-13 18:47:29 -0400

    para vercel

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 from database import db
 from controllers import cliente_controller, producto_controller, venta_controller, compra_controller, main_controller, empleado_controller
 from insert import insertar_empleados 
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Crear tablas si no existen
         insertar_empleados(app)  
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
